c48638901b0889f6941b91daeeb2eda405061ccb
Emilio Zand <emilio.zand@gmail.com>
2018-03-14 00:08:44 -0700

    Fix Exception handling for spotipy

--- matrix.py
+++ matrix.py
@@ -216,28 +216,27 @@ def getWeatherImage():
     return weatherImage
 
 def getSpotifyImage():
-    now_playing = {'is_playing': False}
     try:
         now_playing = sp.current_user_playing_track()
+        if now_playing and now_playing['is_playing']:
+            title = now_playing['item']['name']
+            artists = ''
+            count = 0
+            for artist in now_playing['item']['artists']:
+                if count > 0:
+                    artists += ', '
+                artists += (artist['name'])
+                count += 1
+
+            spotifyImage = Image.open("images/spotify.png").convert('RGB')
+            spotifyDraw = ImageDraw.Draw(spotifyImage)
+            spotifyDraw.text((40,3), title, font=minecraftia, fill=(0, 255, 255))
+            spotifyDraw.text((40,18), artists, font=minecraftia, fill=(255, 0, 255))
+            return spotifyImage
+        else:
+            return None
     except Exception:
         configSpotify()
-
-    if now_playing and now_playing['is_playing']:
-        title = now_playing['item']['name']
-        artists = ''
-        count = 0
-        for artist in now_playing['item']['artists']:
-            if count > 0:
-                artists += ', '
-            artists += (artist['name'])
-            count += 1
-
-        spotifyImage = Image.open("images/spotify.png").convert('RGB')
-        spotifyDraw = ImageDraw.Draw(spotifyImage)
-        spotifyDraw.text((40,3), title, font=minecraftia, fill=(0, 255, 255))
-        spotifyDraw.text((40,18), artists, font=minecraftia, fill=(255, 0, 255))
-        return spotifyImage
-    else:
         return None
 
 def getDriveTime():
